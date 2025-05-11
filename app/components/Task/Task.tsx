85ef3deeacfb8c2567d8ba411e51d8295b70fcfb
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-05-11 17:30:08 +0300

    refactor: enhance Todo component with edit mode, drag-and-drop, and improved UI

--- app/components/Task/Task.tsx
+++ app/components/Task/Task.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useState } from "react";
+import TrashCan from "../../../public/trash-can.svg"
 import { useTodoStore, type Todo } from "@/app/store/store";
 import { STATE_DONE, STATE_ONGOING, STATE_TODO } from "@/app/constants/constants";
+import Image from "next/image";
 
 export default function Todo({ id, title, stateTitle, completed }: Todo) {
 
@@ -9,45 +12,136 @@ export default function Todo({ id, title, stateTitle, completed }: Todo) {
     stateTitle === STATE_ONGOING ? 'border-yellow-300' :
       stateTitle === STATE_DONE ? 'border-green-300' : 'border-gray-500';
 
-  const changeCompleted = useTodoStore(state => state.changeCompleted);
-  const onDragStart = useTodoStore(state => state.onDragStart);
+  const [editMode, setEditMode] = useState(true);
+  const [newTitle, setNewTitle] = useState(title);
+
   const removeTodo = useTodoStore(state => state.removeTodo);
+  const onDragStart = useTodoStore(state => state.onDragStart);
+  const changeTitle = useTodoStore(state => state.changeTitle);
+  const changeCompleted = useTodoStore(state => state.changeCompleted);
+
+  function handleTodoEditModeEdit(event: React.MouseEvent) {
+    event.preventDefault();
+    setEditMode(false);
+
+  }
+
+  function handleTodoEditModeDone(event: React.MouseEvent) {
+    event.preventDefault();
+    setEditMode(true);
+    changeTitle(id, newTitle);
+  }
 
   function handleRemoveTodo(event: React.MouseEvent, id: Todo['id']) {
     event.preventDefault();
     removeTodo(id);
   }
+
   return (
     <div
-      draggable
+      draggable={editMode}
       onDrag={() => onDragStart(id)}
-      className={`flex flex-row justify-between items-baseline p-3 m-3 border-b-2 ${borderColor} hover:cursor-pointer even:bg-cyan-900/75 odd:bg-yellow-600/75 rounded-md`}>
+      className={`flex flex-row justify-between p-3 m-3 border-b-2 ${borderColor} hover:cursor-pointer even:bg-cyan-900/75 odd:bg-yellow-600/75 rounded-md`}>
       <section
         onClick={() => changeCompleted(id)}
-        className="flex justify-center items-center border rounded-full w-4 h-4 p-4 hover:cursor-default">
-        <input
-          type="checkbox"
-          onChange={() => { }}
-          checked={completed}
-        ></input>
+        className="flex flex-col justify-center items-center border rounded-full w-fit h-full p-3 hover:cursor-default">
+        {completed ?
+          <>
+            <label htmlFor='completed'></label>
+            <button type="button" className="border hover:bg-red-700/75 rounded-full p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="h-6 w-6 stroke-current stroke-2">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </> :
+          <>
+            <label htmlFor='completed'></label>
+            <button type="button" className="border hover:bg-green-700/75 rounded-full p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="h-6 w-6 stroke-current stroke-2">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12l6 6L19.5 7" />
+              </svg>
+            </button>
+          </>}
       </section>
-      <section className="flex gap-8">
-        <p className={`${completed && 'line-through'}`}>{title}</p>
+      <section className="flex flex-col  gap-8 w-full mx-3">
+        {editMode ?
+          <>
+            <p className={`${completed && 'line-through'} text-justify  border-2 border-gray-500/75 px-4 min-w-full w-auto h-full`}>{title}</p>
+          </> :
+          <>
+            <input
+              name="title"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              className={`${completed && 'line-through'} text-justify  border-2 min-w-full w-auto h-full border-green-500/75`}>
+            </input>
+          </>
+        }
       </section>
       <section
-        className="flex flex-row justify-center items-center border rounded-full w-4 h-4 p-4 hover:cursor-default"
+        className="flex flex-col gap-3 p-3 justify-center items-center border rounded-full w-fit h-full hover:cursor-default"
       >
+        {editMode ?
+          <button
+            type="button"
+            title="edit button"
+            onClick={handleTodoEditModeEdit}
+            className="hover:bg-blue-700/75 border rounded-full p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 w-6 stroke-current stroke-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 3.487a2.5 2.5 0 00-3.536 0L4.5 11.25V17h5.75l8.826-8.826a2.5 2.5 0 000-3.536l-1.414-1.414z" />
+            </svg>
+          </button> : <button
+            type="button"
+            title="edit button"
+            onClick={handleTodoEditModeDone}
+            className="hover:bg-blue-700/75 border rounded-full p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 w-6 stroke-current stroke-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>}
+        <div className="border w-full"></div>
         <button
+          type="button"
+          title="delete button"
           onClick={(event) => handleRemoveTodo(event, id)}
-          className="hover:bg-red-700/75 rounded-full p-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="h-6 w-6 stroke-current stroke-2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+          className="hover:bg-red-700/75 border rounded-full p-1">
+          <Image about="trash can"
+            width={24}
+            height={24}
+            src={TrashCan.src}
+            alt="trash can"
+            className="h-6 w-6"
+          />
         </button>
       </section>
     </div>
