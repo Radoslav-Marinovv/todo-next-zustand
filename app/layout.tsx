f49e6db88b1549bb3ab98dc53025473e1a2c79f6
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-05-11 19:07:25 +0300

    refactor: remove width and height from Open Graph image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,8 +48,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/opengraph-image.png",
-        width: 1200,
-        height: 630,
         alt: "Todo Next.js Zustand",
       },
     ],
